db56c6f9562bf9a442f3af6ffb264c037b4aa54c
adriarm <adria.rodriguez.munoz@gmail.com>
2025-02-04 17:25:14 -0500

    carousel

--- _pages/fullvm/script.js
+++
@@ -1,7 +0,0 @@
-let scrollAmount = 0;
-function moveGallery(direction) {
-    const gallery = document.getElementById("gallery");
-    const scrollDistance = 220; // Adjust based on image width + margin
-    scrollAmount += direction * scrollDistance;
-    gallery.style.transform = `translateX(${-scrollAmount}px)`;
-}
